3ebcf545e51fc0da6ed90aff3b02ada24100998d
biprajit163 <biprajit163@gmail.com>
2023-11-29 02:02:23 +0000

    remove result file, modify main.py
    updated code to send task to system, and user input via user

--- main.py
+++ main.py
@@ -1,34 +1,17 @@
 import openai
 import os
-import sys
-  
-try:
-  openai.api_key = os.environ['OPENAI_API_KEY']
-except KeyError:
-  sys.stderr.write("""
-  You haven't set up your API key yet.
-  
-  If you don't have an API key yet, visit:
-  
-  https://platform.openai.com/signup
+import sys 
 
-  1. Make an account or sign in
-  2. Click "View API Keys" from the top right menu.
-  3. Click "Create new secret key"
+openai.api_key = os.environ['OPENAI_API_KEY']
 
-  Then, open the Secrets Tool and add OPENAI_API_KEY as a secret.
-  """)
-  exit(1)
+text = "Hey how are you, what did you do today? I saw a beautiful sunset in the park."
+target_language = "portuguese"
 
 response = openai.ChatCompletion.create(
-  model="gpt-4", # only available if OpenAI has given you early access, otherwise use: "gpt-3.5-turbo"
-  # 32K context gpt-4 model: "gpt-4-32k"
-  messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
+    model="gpt-3.5-turbo", 
+    messages=[
+        {"role": "system", "content": f"translate the following English text into {target_language}"},
+        {"role" : "user", "content": text}
     ]
 )
-
-print(response)
+print(response['choices'][0]['message']['content'].strip())
